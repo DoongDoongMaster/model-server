ec538e2d3202ef73420c56fe40247504f5f36ee1
bsa0322 <bsa0322@ewhain.net>
2024-06-18 13:05:30 +0900

    [WIP] omr model serving

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM python:3.11.5
 
+# Install the necessary system dependencies
+RUN apt-get update && apt-get install -y \
+    libgl1-mesa-glx \
+    && rm -rf /var/lib/apt/lists/*
+    
 WORKDIR /home/code/automatic-drum-transcription
 
 COPY ../automatic-drum-transcription/requirements.txt ./requirements.txt
@@ -7,5 +12,11 @@ COPY ../automatic-drum-transcription/requirements.txt ./requirements.txt
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
 
+WORKDIR /home/code/optical-music-recognition
+
+COPY ../optical-music-recognition/requirements.txt ./requirements.txt
+
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
 
 WORKDIR /home/code/model-server
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 import io
+import cv2
 import logging
 import sys, os
+import xml.etree.ElementTree as ET
 
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 
 # 2 계층 위의 상위 폴더 접근
 sys.path.append(f'{os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))}/automatic-drum-transcription/src')
+# sys.path.append(f'{os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))}/optical-music-recognition/ddm-omr')
 print(sys.path)
 
 from constant import (
@@ -14,7 +17,7 @@ from constant import (
 )
 from feature.feature_extractor import FeatureExtractor
 from serving.model_serving import ModelServing
-
+import infer
 
 app = FastAPI()
 # 로그 생성
@@ -35,10 +38,10 @@ model_serving_class = ModelServing(
 
 @app.post("/adt/predict", tags=["ADT"])
 def model_predict(file: bytes = File(...)):
-    logger.info("I recived API server POST request")
+    logger.info("[ADT] I recived API server POST request")
     # ============ sercved model class create ========================
 
-    logger.info("I'm waiting model prediction")
+    logger.info("[ADT] I'm waiting model prediction")
     # # Implement model predict logic
     bytesIO = io.BytesIO(file)
     audio = FeatureExtractor.load_audio(bytesIO)
@@ -53,3 +56,44 @@ def model_predict(file: bytes = File(...)):
         "onsets_arr": onsets_arr,
         "audio_total_sec": audio_total_sec,
     }
+
+# 저장할 디렉토리 경로 설정
+UPLOAD_DIRECTORY = "./uploaded_files/"
+
+# 디렉토리가 존재하지 않으면 생성
+if not os.path.exists(UPLOAD_DIRECTORY):
+    os.makedirs(UPLOAD_DIRECTORY)
+
+
+@app.post("/omr/predict", tags=["OMR"])
+async def omr_predict(file: UploadFile = File(...)):
+    logger.info("[OMR] I recived API server POST request")
+    # ============ sercved model class create ========================
+
+    try:
+        # 업로드된 파일의 이름을 가져옴
+        filename = file.filename
+        
+        # 파일 저장 경로 설정
+        file_location = os.path.join(UPLOAD_DIRECTORY, filename)
+
+        # 파일 저장
+        with open(file_location, "wb") as f:
+            f.write(await file.read())
+
+        img = cv2.imread(file_location, cv2.IMREAD_UNCHANGED)
+        print(img)
+        
+        tree = infer.inference(img)
+
+        # BytesIO 객체를 사용하여 XML 트리를 바이트 데이터로 변환
+        byte_io = io.BytesIO()
+        tree.write(byte_io, encoding="utf-8", xml_declaration=True)
+
+        # 바이트 데이터를 가져옴
+        byte_data = byte_io.getvalue()
+
+        return {"result": byte_data}
+    
+    except Exception as e:
+        return {"error": str(e)}
